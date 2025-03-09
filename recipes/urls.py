6fc15b8c2c07093c61aed17dc6176a65038fe925
Samuel Brasileiro Azambuja Oliveira <sbrasileiro4@gmail.com>
2025-03-09 19:26:49 -0300

    remoções

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from recipes.views import Home, Contato, Sobre
+from recipes.views import Home
 urlpatterns = [
-    path('sobre/', Sobre),
-    path('contato/',Contato),
     path('',Home)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-# from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def Home(request):
     return render(request,'recipes/home.html',context={
         'name': 'Samuel Oliveira'
     })
-def Contato(request):
-    return render(request, 'recipes/contato.html')
-def Sobre(request):
-    return HttpResponse('sobre')
