b9bc91c2d9785b610f7b991f8e4572e1636af23b
Samuel Brasileiro Azambuja Oliveira <sbrasileiro4@gmail.com>
2025-03-21 19:49:19 -0300

    Adicionando novas alterações

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-    return render(request,'recipes/home.html',context={
+    return render(request,'recipes/page/home.html',context={
         'name': 'Samuel Oliveira'
     })
